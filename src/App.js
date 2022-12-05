b06274c342842657c6a495c05b346c679edead7b
Sophie Roudier <sophie.roudier@hotmail.fr>
2022-12-05 15:26:10 +0100

    Router configuration

--- src/App.js
+++ src/App.js
@@ -1,2 +1,24 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Error from "./components/Error";
+import Header from "./components/Header";
+import SingleCocktail from "./pages/SingleCocktail";
+
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/cocktail/:id" element={<SingleCocktail />}></Route>
+        <Route path="*" element={<Error />}></Route>
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
